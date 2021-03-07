3b75a07479ba61b22db2fe87bed6eeadb262e99e
kimsuraaaa <kimsuraaaa@gmail.com>
2021-03-07 19:26:38 +0900

    20210307_image 최적화 및 이력 내용 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-/* 2021-02-21, ver 1.2 */
+/* 2021-03-07, ver 1.3 */
 function scrollCheck(){
     var idx = $(document).scrollTop();
     if(idx > 1){
@@ -25,9 +25,22 @@ function windowResize(){
     $('main > ul > li').css('min-height',$(window).height());
 }
 
+function deviceCheck() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    if ((userAgent.search("iphone") > -1) || (userAgent.search("ipod") > -1) || (userAgent.search("ipad") > -1)) {
+        window.open('https://itunes.apple.com/kr/app/id1539582420', '_blank');
+        $('.device-aos').remove();
+    } else if (userAgent.search("android") > -1){
+        $('.device-ios').remove();
+    } else {
+        $('.device-ios').remove();
+    }
+}
+
 $(document).ready(function(){
     scrollCheck();
     windowResize();
+    deviceCheck();
     $(window).resize(windowResize);
 
     $(document).on('scroll',function(){
